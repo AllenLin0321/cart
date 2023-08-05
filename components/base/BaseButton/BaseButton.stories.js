cae9ebd9435a15383c843723a56a3f4f54833ce9
林逸倫 <glux7381@gmail.com>
2023-08-05 21:19:46 +1000

    feat: init storybook

---
+++ components/base/BaseButton/BaseButton.stories.js
@@ -0,0 +1,25 @@
+﻿import BaseButton from './index'
+export default {
+  title: 'Base/Button',
+  component: BaseButton,
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary'],
+      },
+      defaultValue: 'primary',
+    },
+    round: { control: 'boolean' },
+    text: {
+      control: 'text',
+      defaultValue: 'Button',
+    },
+  },
+}
+
+export const Button = (arg, { argTypes }) => ({
+  components: { BaseButton },
+  props: Object.keys(argTypes),
+  template: '<BaseButton v-bind="$props" />',
+})
---
+++ components/base/TheSpinner/TheSpinner.stories.js
@@ -0,0 +1,25 @@
+﻿import TheSpinner from './index'
+export default {
+  title: 'Base/Spinner',
+  component: TheSpinner,
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary'],
+      },
+      defaultValue: 'primary',
+    },
+    round: { control: 'boolean' },
+    text: {
+      control: 'text',
+      defaultValue: 'Button',
+    },
+  },
+}
+
+export const Spinner = (arg, { argTypes }) => ({
+  components: { TheSpinner },
+  props: Object.keys(argTypes),
+  template: '<TheSpinner v-bind="$props" />',
+})
