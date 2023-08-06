bcfb189aa976fd0031f6702f1e754bdce6686c65
林逸倫 <glux7381@gmail.com>
2023-08-06 14:40:02 +1000

    feat: 添加BaseButton 的Test Case

---
+++ components/base/BaseButton/BaseButton.spec.js
@@ -0,0 +1,13 @@
+﻿import { mount } from '@vue/test-utils'
+import BaseButton from '@/components/base/BaseButton'
+
+describe('BaseButton', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(BaseButton, {
+      propsData: {
+        text: 'Button',
+      },
+    })
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
